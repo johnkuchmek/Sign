ab7134d69a4103985ba3815b50dd7ac78ccd8ee0
johnkuchmek <52086986+johnkuchmek@users.noreply.github.com>
2021-09-14 13:18:36 -0400

    Update sign.py

--- sign.py
+++ sign.py
@@ -5,14 +5,14 @@ from fastecdsa import curve, ecdsa, keys, point
 from hashlib import sha256
 
 def sign(m):
-	#generate public key
-	#Your code here
-	priv_key, public_key = keys.gen_keypair(curve.secp256k1)
+    #generate public key
+    #Your code here
+    priv_key, public_key = keys.gen_keypair(curve.secp256k1)
   
-	sig= priv_key.sign(m)
-  	r = sig[0]
- 	s = sig[1]
-	assert isinstance( public_key, point.Point )
-	assert isinstance( r, int )
-	assert isinstance( s, int )
-	return( public_key, [r,s] )
+    sig= priv_key.sign(m)
+    r = sig[0]
+    s = sig[1]
+    assert isinstance( public_key, point.Point )
+    assert isinstance( r, int )
+    assert isinstance( s, int )
+    return( public_key, [r,s] )
