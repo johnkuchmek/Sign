da1d4850eacc48149dabed29768441013cb877ca
johnkuchmek <52086986+johnkuchmek@users.noreply.github.com>
2021-09-15 21:59:19 -0400

    Update sign.py

--- sign.py
+++ sign.py
@@ -8,13 +8,10 @@ def sign(m):
     #generate public key
     #Your code here
     priv_key, pub_key = keys.gen_keypair(curve.secp256k1) 
-    print(priv_key)
-    print(pub_key)
     (r,s) = ecdsa.sign(m,priv_key,secp256k1,hashfunc=sha256)
-    print((r,s))
-    valid = ecdsa.verify((r,s),m,pub_key, secp256k1)
-    print(valid)
+    
     assert isinstance( pub_key, point.Point )
     assert isinstance( r, int )
     assert isinstance( s, int )
+    
     return(pub_key, [r,s])
